c473c004b219606fd74a3449bdc2751d46f4c82e
GeorgePopoff <popoff96@live.com>
2016-12-28 21:24:18 +0300

    XML rpc version

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,18 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Client {
+
+    private static PrintWriter serverOut;
+    private static BufferedReader serverIn;
+
     public static void main(String[] args){
         if ( args.length < 2 ) {
             printUsage();
@@ -22,12 +32,40 @@ public class Client {
                         new InputStreamReader(System.in))
         ) {
 
+            Client.serverOut = out;
+            Client.serverIn = in;
+
             System.out.println("> Connected!");
 
             String userInput;
             String serverOutput;
 
             System.out.print("> Enter server login and password: ");
+
+            boolean isAuthorized = false;
+
+            // AUTHORIZATION
+            while ( !isAuthorized ) {
+                try {
+                    userInput = sysin.readLine();
+                    serverOutput = authorize(userInput);
+
+                    if ( serverOutput.contains("rcl error") ) {
+                        System.out.println(args[0] + ": " + serverOutput);
+                        continue;
+                    }
+
+                    isAuthorized = true;
+
+                    System.out.println("> Authorized successful");
+                    System.out.println(args[0] + ": " + serverOutput);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String[] requestParts;
+            // DIALOG
             while ( (userInput = sysin.readLine()) != null ) {
 
                 if ( socket.isClosed() || !socket.isConnected() ) {
@@ -35,30 +73,26 @@ public class Client {
                 }
 
                 if ( userInput.equals("exit") ) {
-                    userInput = "disconnect";
-                }
-
-                out.println(userInput);
-
-                if ( userInput.equals("disconnect") ) {
+                    userInput = "rcl.disconnect";
+                } else if ( userInput.equals("disconnect") ) {
                     socket.close();
                     break;
                 }
 
-                while ( !(serverOutput = in.readLine()).equals("EOT") ) {
+                requestParts = userInput.split(" ");
+                if ( requestParts.length == 1 ) {
+                    serverOutput = invokeFunction(userInput, null);
+                } else {
+                    String methodName = requestParts[0];
+                    String[] params = new String[requestParts.length - 1];
 
-                    if (serverOutput.equals("null")) {
-                        System.out.println("> Server not responding...");
-                        out.println("status");
-
-                        if (serverOutput.equals("null")) {
-                            System.out.println("> Server is down.");
-                            break;
-                        }
+                    for (int i = 1; i < requestParts.length; ++i) {
+                        params[i-1] = requestParts[i];
                     }
-
-                    System.out.println(args[0] + ": " + serverOutput);
+                    serverOutput = invokeFunction(methodName, params);
                 }
+
+                System.out.println(args[0] + ": " + serverOutput);
 
                 System.out.print("rcl@client $ ");
             }
@@ -66,6 +100,9 @@ public class Client {
         } catch (IOException e) {
             System.err.println("! An error occurred: " + e);
             //e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("! An error occurred: " + e);
+            e.printStackTrace();
         }
 
         System.out.println("> Disconnecting...");
@@ -74,4 +111,88 @@ public class Client {
     private static void printUsage() {
         System.out.println("RCLClient usage: rclclient.jar host port");
     }
+
+    /**
+     * Send xml-rpc authorize
+     * message
+     *
+     * @retur String about success auth when okay
+     *  and exception if result was failed
+     */
+    private static String authorize(String data) throws Exception {
+        String[] parts = data.split(" ");
+
+        String username, password;
+        try {
+            username = parts[0].trim();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            username = " ";
+        }
+
+        try {
+            password = parts[1].trim();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            password = " ";
+        }
+
+        Object[] params = { username, password };
+
+        String result;
+        try {
+            result = invokeFunction("rcl.authorize", params);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return result;
+    }
+
+    private static String invokeFunction(String name, Object[] params)
+        throws Exception {
+        String result = null;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element body = document.createElement("methodCall");
+        document.appendChild(body);
+
+        Element methodName = document.createElement("methodName");
+        methodName.setTextContent(name);
+        body.appendChild(methodName);
+
+        if ( params != null ) {
+            Element paramsElement = document.createElement("params");
+            for (int i = 0; i < params.length; ++i) {
+                Element param = document.createElement("param");
+                Element value = document.createElement("value");
+                Element type = document.createElement("string"); // TODO: other param types
+
+                paramsElement.appendChild(param);
+                param.appendChild(value);
+                value.appendChild(type);
+                type.setTextContent(params[i].toString());
+            }
+            body.appendChild(paramsElement);
+        }
+
+        String serialized = XMLUtil.documentToText(document);
+
+        serverOut.println(serialized);
+        serverOut.println("EOT");
+
+        String serverResponse = null;
+
+        StringBuilder response = new StringBuilder();
+        while ( !(serverResponse = serverIn.readLine()).equals("EOT") ) {
+            response.append(serverResponse);
+            response.append('\n');
+        }
+
+        result = XMLUtil.getResultOf(response.toString());
+
+        return result;
+    }
 }
+
